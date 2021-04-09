7e11bbc097ab176352cc40e46da5c1b1cb1000bd
kc <6568057@qq.com>
2021-04-09 16:18:08 +0800

    rhtest8

--- frp.sh
+++ frp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /root/rhtest
-crontab -l |grep frp.sh || echo "0/5 * * * * /root/repo/rhtest/frp.sh ">>/var/spool/cron/root
+crontab -l |grep frp.sh || echo "0/5 * * * * /root/rhtest/frp.sh ">>/var/spool/cron/root
 
 if ps -ef |grep -v frp.log &>/dev/null;then
    nohup $PWD/frpc -c ./frpc.ini >/tmp/frp.log 2>&1 &
