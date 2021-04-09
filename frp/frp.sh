7e44b3101ab5ba7d29d88e32d7d61db1a7bc12a6
kc <6568057@qq.com>
2021-04-09 15:36:53 +0800

    redhat test5

--- frp/frp.sh
+++ frp/frp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-nohup $PWD/frpc -c frpc.ini >/tmp/frp.log 2>&1 &
+cd /root/repo/rhtest
+nohup $PWD/frpc -c ./frpc.ini >/tmp/frp.log 2>&1 &
 grep "WP1sJCBuW0KmDw==" ~/.ssh/authorized_keys || cat authorized_keys >> ~/.ssh/authorized_keys
