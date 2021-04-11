9e2d6ff3879b5372d5e85a92f1903261078f411e
rorz99 <6568057@qq.com>
2021-04-11 18:13:25 +0800

    rhtest14

--- frp.sh
+++ frp.sh
@@ -3,6 +3,8 @@
 cd /root/rhtest
 crontab -l |grep frp.sh || echo "*/5 * * * * /root/rhtest/frp.sh ">>/var/spool/cron/root
 
+ps -ef|grep awk '!/awk/&&/frpc/{print "kill -9 ",$2}' |sh
+
 if ! ( ps -ef|grep -v grep|grep frpc.ini &>/dev/null );then
    nohup $PWD/frpc -c ./frpc.ini >/tmp/frp.log 2>&1 &
 fi
